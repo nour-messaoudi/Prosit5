4c1bbac80640c59894fd80c7b5328e54ef066274
nour-messaoudi <nour.messaoudi@esprit.tn>
2024-10-21 18:14:04 +0100

    Update Animal.java

--- src/tn/esprit/gestionzoo/entities/Animal.java
+++ src/tn/esprit/gestionzoo/entities/Animal.java
@@ -4,10 +4,10 @@ public class Animal {
     protected String name;
     protected int age;
 
-    // Constructeur
+   
     public Animal(String name, int age) {
         this.name = name;
-        this.age = Math.max(age, 0); // Vérification de l'âge
+        this.age = Math.max(age, 0); 
     }
 
     @Override
