3269999f37464ef5b53039431d26321b9c7dde15
nour-messaoudi <nour.messaoudi@esprit.tn>
2024-10-21 18:14:35 +0100

    Update Aquatic.java

--- src/tn/esprit/gestionzoo/entities/Aquatic.java
+++ src/tn/esprit/gestionzoo/entities/Aquatic.java
@@ -3,13 +3,13 @@ package tn.esprit.gestionzoo.entities;
 public class Aquatic extends Animal {
     protected String habitat;
 
-    // Constructeur
+    
     public Aquatic(String name, int age, String habitat) {
-        super(name, age); // Appel du constructeur de Animal
+        super(name, age); 
         this.habitat = habitat;
     }
 
-    // Méthode swim()
+  
     public void swim() {
         System.out.println("This aquatic animal is swimming.");
     }
